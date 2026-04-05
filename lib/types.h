170d9e448d52c58c5a5db3581052279156e57471
RahRah101 <158766553+RahRah101@users.noreply.github.com>
2026-04-05 12:00:01 -0400

    Added global type definition.
    I understand the instinct from the previous code of avoiding global namespace pollution, but in this case I feel it's justified.
    
    Tracktype is leaking out of Kdenfile everywhere with calls to KdenliveFile::VIDEO, KdenliveFile::AUDIO and KdenliveFile::TrackType all over the place, especially in KdenCLIProject(or, previously, KdenliveProject).
    
    With my CLI layer in kdencli.cpp, I realized that the CLI layer needed these types too(while trying to implement auto A/V split when user adds a video with both streams) and felt it was weird do rely on tracktype from kdenlivefile here.
    
    When a type is used by every layer, scoping it to the lowest level layer doesn't protect the namespace, it just creates a dependency in every other layer on Kdenlivefile:: for TrackType. That's the opposite of what encapsulation is supposed to do.
    
    The real question in my opinion is whether or not this type belong to the Kdenlivefile and my answer, which might be wrong, is no. TrackType is a domain concept, video vs audio is a property of the problem space, not a property of XML manipulation. The fact that KdenliveFile, KdenCLIProject, KdenliveProject, and kdencli.cpp all need it is, in and of itself, proof that it does not belong to any individual one. The only scenario where I feel the past implementation would be justified was if KdenliveFile was a self-contained library with no layers above it, but the creation of the KdenliveProject object(and, subsequently, KdenCLIProject) changed this. So I'm sorry Josh, but we are cluttering the global namespace today.

---
+++ lib/types.h
@@ -0,0 +1,14 @@
+#ifndef KDENCLI_TYPES_H
+#define KDENCLI_TYPES_H
+
+typedef int ClipId;
+typedef int TrackId;
+typedef int TrackEntryId;
+
+// Data types that I want cluttering the global namespace (Sorry Josh!)
+enum class TrackType { VIDEO, AUDIO };
+
+// CLI place behavior
+enum class PlaceMode { AUTO, VIDEO_ONLY, AUDIO_ONLY };
+
+#endif
